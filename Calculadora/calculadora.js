f9b637fc141be3d05c9ea06cb95f0a1d6dc3dfb2
brunoss.contato@gmail.com <brunoss.contato@gmail.com>
2023-04-14 15:31:56 -0300

    final

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,20 @@
+const entrada1 = prompt("Informe o primeiro número:")
+const entrada2 = prompt("Informe o segundo número:")
+
+
+const x = parseFloat(entrada1)
+const y = parseFloat(entrada2)
+
+const soma = x + y
+const subtração = x - y
+const multipliação = x * y
+const divisão = x / y
+
+
+alert(
+"Resultados:\n" +
+"\nSoma: " + soma +
+"\nSubtração: " + subtração + 
+"\nMultiplicação: " + multipliação +
+"\nDivisão: " + divisão
+)
